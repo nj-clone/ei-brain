256f439a9880650ffdd3b532c79b687ec16c1f4b
nj-clone <dj.nazzon@gmail.com>
2026-03-05 15:49:34 +0500

    Create firebase.py

---
+++ app/firebase.py
@@ -0,0 +1,12 @@
+import os
+import json
+import firebase_admin
+
+from firebase_admin import credentials, firestore
+
+if not firebase_admin._apps:
+    firebase_json = os.getenv("FIREBASE_KEY_JSON")
+    cred = credentials.Certificate(json.loads(firebase_json))
+    firebase_admin.initialize_app(cred)
+
+db = firestore.client()
